099d76409c128c43bf8ba8e650e51b70922a6af6
jose mendoza <mendozarriff@gmail.com>
2017-09-28 13:26:01 -0700

    react-create-app on Heroku

---
+++ src/Components/Range.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+
+class Range extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            value: props.value
+        }
+    }
+    static defaultProps = {
+        min: 0,
+        max:245,
+        step: 1
+    }
+    onChange(event){
+        this.props.onChange(this.state.value);
+        this.setState({value:event.target.value});
+    }
+  render() {
+    return (
+      <div className="range">
+          <input type='range' 
+          value = {this.state.value}
+          min = {this.props.min}
+          max = {this.props.max}
+          step = {this.props.step}
+          onChange = {this.onChange.bind(this)}
+          />
+      </div>
+    );
+  }
+}
+
+
+export default Range;
